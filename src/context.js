47104356bd2aaeb5b8a651450544e6ca46c9da63
JX Wang <wjianxu@gmail.com>
2019-05-09 02:44:32 +0800

    feat: 增加命令行提示配置

---
+++ src/context.js
@@ -0,0 +1,20 @@
+/**
+ * metadata 文件夹名称
+ *
+ * @type {string}
+ */
+exports.METADATA_FOLDER = '$$t2e.data';
+
+/**
+ * METADATA_FOLDER 下的 metadata 文件
+ *
+ * @type {string}
+ */
+exports.METADATA_YAML = 'metadata.yml';
+
+/**
+ * METADATA_FOLDER 下的 uuid 文件
+ *
+ * @type {string}
+ */
+exports.UUID_YAML = 'uuid.yml';
---
+++ src/init.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const os = require('os');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const pkg = require('../package');
+const fileUtils = require('./utils/fileUtils');
+const metadata = require('./metadata');
+
+const pkgVersion = pkg.version;
+console.log(chalk.bold.cyan(`\n...Txt2Eput ${pkgVersion} metadata init...\n`));
+
+const folderQuestion = {
+    type: 'input',
+    name: 'folder',
+    message: '请输入 txt 所在目录（此目录必须已经存在）：',
+    validate: function validate(input) {
+        const isFolder = fileUtils.existFolder(input) === fileUtils.FolderMode.NORMAL;
+        if (isFolder) {
+            return isFolder;
+        }
+
+        return chalk.bold.red('不是有效路径，请重新输入');
+    },
+};
+
+const titleQuestion = {
+    type: 'input',
+    name: 'title',
+    message: '请输入 epub 书名：',
+    default: function def(answers) {
+        return path.basename(answers.folder);
+    },
+};
+
+const questions = [
+    folderQuestion,
+    titleQuestion,
+    {
+        type: 'input',
+        name: 'author',
+        message: '请输入作者：',
+        default: function def() {
+            return os.userInfo().username;
+        },
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: '请输入简介（可以为空）：',
+    },
+    {
+        type: 'confirm',
+        name: 'confirm',
+        message: '确认上述信息正确?',
+        default: true,
+    },
+];
+
+inquirer.prompt(questions).then(answers => {
+    console.log();
+    console.log(answers);
+    if (!answers.confirm) {
+        console.log(chalk.bold.red('放弃初始化!'));
+    } else {
+        const {folder, ...meta} = answers;
+        metadata.init(folder, meta);
+    }
+
+    console.log();
+});
