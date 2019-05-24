29a7e782fb2b3510fe8b81644ce616f1d2d3985d
JX Wang <wjianxu@gmail.com>
2019-05-24 22:11:36 +0800

    refactor: 采用 Map 定义分隔规则
    好处是可以在引用的时候限定 key 值，Object 无法在引用的时候控制 key 值范围

--- src/split/regexRules.ts
+++ src/split/regexRules.ts
@@ -1,12 +1,10 @@
-interface RegexRule {
+interface RuleItem {
     title: string;
     description: string;
     re: RegExp;
 }
 
-export interface RuleItems {
-    [key: string]: RegexRule;
-}
+export type RuleItemKey = 'zhChapter' | 'num' | 'zhNum';
 
 /**
  * 章节检测正则。
@@ -15,22 +13,31 @@ export interface RuleItems {
  *
  * 由于章节很多人划分的随心所欲，很难全部匹配，所以这儿仅仅实现这么几种，如果不符合要求，还需要先手工修改下文档。
  */
-const rules: RuleItems = {
-    zhChapter: {
-        title: '中文章节',
-        description: '第 x 章、第 x 节 xxx',
-        re: /^\s*([第笫].{1,6}[章节卷集部篇回].{0,30})\s*$/gm,
-    },
-    num: {
-        title: '数字',
-        description: '1, 2 xxx, 3...',
-        re: /^\s*(\d{1,6}.{0,30})\s*$/gm,
-    },
-    zhNum: {
-        title: '中文数字',
-        description: '一、二 xxx、三...',
-        re: /^\s*([一二三四五六七八九十百千]{1,6}.{0,30})\s*$/gm,
-    },
-};
+const rules: Map<RuleItemKey, RuleItem> = new Map<RuleItemKey, RuleItem>([
+    [
+        'zhChapter',
+        {
+            title: '中文章节',
+            description: '第 x 章、第 x 节 xxx',
+            re: /^\s*([第笫].{1,6}[章节卷集部篇回].{0,30})\s*$/gm,
+        },
+    ],
+    [
+        'num',
+        {
+            title: '数字',
+            description: '1, 2 xxx, 3...',
+            re: /^\s*(\d{1,6}.{0,30})\s*$/gm,
+        },
+    ],
+    [
+        'zhNum',
+        {
+            title: '中文数字',
+            description: '一、二 xxx、三...',
+            re: /^\s*([一二三四五六七八九十百千]{1,6}.{0,30})\s*$/gm,
+        },
+    ],
+]);
 
 export default rules;
