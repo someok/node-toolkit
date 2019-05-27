a2336ea6dca7fbe76b318a286ef6ac043ffe3444
JX Wang <wjianxu@gmail.com>
2019-05-27 13:03:35 +0800

    chore: 升级依赖到最新版

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,25 @@
 // 参照： https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
 //
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'prettier/@typescript-eslint',
-    // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'plugin:prettier/recommended',
-  ],
-  env: {
-    es6: true,
-    node: true,
-  },
-  parserOptions: {
-    // 支持最新 JavaScript
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    // '@typescript-eslint/explicit-member-accessibility': 0,
-  },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'prettier/@typescript-eslint',
+        // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'plugin:prettier/recommended',
+    ],
+    env: {
+        es6: true,
+        node: true,
+    },
+    parserOptions: {
+        // 支持最新 JavaScript
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        // '@typescript-eslint/explicit-member-accessibility': 0,
+    },
 };
