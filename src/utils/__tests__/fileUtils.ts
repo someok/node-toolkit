b7a3800dbd172c2284b447e79d09d54994b50365
JX Wang <wjianxu@gmail.com>
2019-05-28 13:29:28 +0800

    refactor: 删除 readUtf8OrGbkReadFile 方法以及对应的 iconv 依赖

--- src/utils/__tests__/fileUtils.ts
+++ src/utils/__tests__/fileUtils.ts
@@ -1,25 +1,5 @@
 import path from 'path';
-import {readUtf8OrGbkReadFile, checkEncodeing, readAsUtf8String} from '../fileUtils';
-
-function encFile(name: string): string {
-    return path.join(__dirname, 'txt-files/encoding', name);
-}
-
-test('readUtf8OrGbkReadFile', (): void => {
-    let txt = readUtf8OrGbkReadFile(encFile('utf8.txt'), false);
-    expect(txt.trim()).toBe('中文');
-
-    txt = readUtf8OrGbkReadFile(encFile('big5.txt'), false);
-    // console.log(txt);
-    // expect(txt.trim()).toBe('中文');
-    expect(txt).toContain('安保處');
-
-    txt = readUtf8OrGbkReadFile(encFile('gbk.txt'), false);
-    expect(txt.trim()).toBe('中文');
-
-    txt = readUtf8OrGbkReadFile(encFile('gb18030.txt'));
-    expect(txt.trim()).toBe('中文');
-});
+import {checkEncodeing, readAsUtf8String} from '../fileUtils';
 
 test('checkEncodeing', (): void => {
     const files = ['big5.txt', 'gb18030.txt', 'gbk.txt', 'utf8.txt', 'utf8-bom.txt', 'utf16.txt'];
--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -2,9 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import klawSync from 'klaw-sync';
 import chardet from 'chardet';
-import {Iconv} from 'iconv';
 import encoding from 'encoding';
-import {logError} from '@someok/node-utils/lib/logUtils';
 import {createTempFolder as tempFolder} from '@someok/node-utils/lib/fileUtils';
 import Result from '@someok/node-utils/lib/Result';
 
@@ -12,52 +10,6 @@ import {FOLDER_PREFIX} from '../context';
 
 export function createTempFolder(): string {
     return tempFolder('t2e-');
-}
-
-const gbk2utf8 = new Iconv('gbk', 'utf-8');
-const big52utf8 = new Iconv('big5', 'utf-8');
-
-/**
- * 读取 gbk 或 utf8 格式文件，由于 node 默认不支持 gbk，所以使用 Iconv 来做转换。
- *
- * gbk 或 big5 格式检测是使用 Iconv 的转换异常来处理，判断顺序如下：
- *    1、先用 gbk 编码转换成 utf8，如果抛出异常
- *    2、接着用 big5 编码转换成 utf8，如果抛出异常
- *    3、最后则表示为 utf8
- *
- * 这种搞法其实很不科学，可惜 node 里面判断编码的方式很讨厌，用了多个检测包都不好使。
- *
- * todo: 各种 encoding 的读取需要优化
- *
- * @param file 文件路径
- * @param debug true 的时候会输出异常信息
- * @return {string} 文件内容，utf8 格式
- */
-export function readUtf8OrGbkReadFile(file: string, debug: boolean = false): string {
-    const orignBuffer = fs.readFileSync(file);
-
-    let covertedBuffer;
-    try {
-        covertedBuffer = gbk2utf8.convert(orignBuffer);
-    } catch (e) {
-        if (debug) {
-            logError('gbk2utf8:');
-            console.log(e);
-        }
-
-        try {
-            covertedBuffer = big52utf8.convert(orignBuffer);
-        } catch (e) {
-            if (debug) {
-                logError('big52utf8:');
-                console.log(e);
-            }
-
-            covertedBuffer = orignBuffer;
-        }
-    }
-
-    return covertedBuffer.toString();
 }
 
 /**
@@ -97,6 +49,11 @@ export function checkEncodeing(file: string): Result<string> {
     return new Result<string>(isSupport, '不是支持的编码', data);
 }
 
+/**
+ * 读取给定文本文件，并检测编码，如果是支持的编码则转换为 utf8，否则返回错误提示。
+ *
+ * @param file 文件路径
+ */
 export function readAsUtf8String(file: string): Result<string> {
     const encResult = checkEncodeing(file);
     if (!encResult.success) {
