885c35618e4813538c86690f53b56faa6ec1ff95
JX Wang <wjianxu@gmail.com>
2020-05-09 22:35:11 +0800

    chore: 优化 commitlint 配置

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    '*.{ts,tsx}': ['eslint --fix', 'git add'],
-    '*.{js,jsx}': ['eslint --fix', 'git add'],
-    '*.{md,html,json}': ['prettier --write', 'git add'],
-    '*.{css,scss,less}': ['prettier --write', 'git add'],
+    '*.{ts,tsx}': ['eslint --fix'],
+    '*.{js,jsx}': ['eslint --fix'],
+    '*.{md,html,json}': ['prettier --write'],
+    '*.{css,scss,less}': ['prettier --write'],
 };
