92e38aec42f5aff3611ca2fe7114c5c1b0ed36e0
JX Wang <wjianxu@gmail.com>
2020-10-26 11:14:55 +0800

    fix: 删除无用的 export

--- packages/comic-spider/src/main.ts
+++
@@ -1,6 +0,0 @@
-import RemoteImage from './spider/RemoteImage';
-export {fetchAndOutputImages} from './spider/output';
-export {writeUrl2ReadmeTxt} from './spider/readme';
-export {readEnv} from './spider/envConfig';
-
-export {RemoteImage};
