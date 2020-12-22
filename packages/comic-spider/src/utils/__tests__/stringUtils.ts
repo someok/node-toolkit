012b82016ac87e6350833f23a31bcad3398aeeba
JX Wang <wjianxu@gmail.com>
2020-12-22 23:14:24 +0800

    feat: 优化字符串中括号解析，支持中括号、小括号，并支持不完整匹配的括号

--- packages/comic-spider/src/utils/__tests__/stringUtils.ts
+++ packages/comic-spider/src/utils/__tests__/stringUtils.ts
@@ -1,8 +1,8 @@
 import {splitByBracket, escapeWindowsFilename} from '../stringUtils';
 
 test('splitByBracket', () => {
-    const str = '[aaa][bbb]c[cc]ccc[ddd][eee]';
-    const arr = splitByBracket(str);
+    let str = '[aaa][bbb]c[cc]ccc[ddd][eee]';
+    let arr = splitByBracket(str);
     expect(arr.length).toBe(7);
     expect(arr[0]).toEqual({isBracket: true, text: '[aaa]'});
     expect(arr[1]).toEqual({isBracket: true, text: '[bbb]'});
@@ -11,6 +11,27 @@ test('splitByBracket', () => {
     expect(arr[4]).toEqual({isBracket: false, text: 'ccc'});
     expect(arr[5]).toEqual({isBracket: true, text: '[ddd]'});
     expect(arr[6]).toEqual({isBracket: true, text: '[eee]'});
+
+    str = '[123[aaa]（bbb)c(ccccc[ddd][eee]fff(ggg[hhh]iii)jjj]';
+    arr = splitByBracket(str);
+    // console.log(str);
+    // console.log(arr);
+    expect(arr[0]).toEqual({isBracket: false, text: '[123'});
+    expect(arr[1]).toEqual({isBracket: true, text: '[aaa]'});
+    expect(arr[2]).toEqual({isBracket: true, text: '(bbb)'});
+    expect(arr[3]).toEqual({isBracket: false, text: 'c'});
+    expect(arr[4]).toEqual({isBracket: false, text: '(ccccc'});
+    expect(arr[5]).toEqual({isBracket: true, text: '[ddd]'});
+    expect(arr[6]).toEqual({isBracket: true, text: '[eee]'});
+    expect(arr[7]).toEqual({isBracket: false, text: 'fff'});
+    expect(arr[8]).toEqual({isBracket: false, text: '(ggg'});
+    expect(arr[9]).toEqual({isBracket: true, text: '[hhh]'});
+    expect(arr[10]).toEqual({isBracket: false, text: 'iii'});
+    expect(arr[11]).toEqual({isBracket: false, text: ')'});
+    expect(arr[12]).toEqual({isBracket: false, text: 'jjj'});
+    expect(arr[13]).toEqual({isBracket: false, text: ']'});
+
+    // console.log(splitByBracket('[aaa][bbb]ccc[ddd][eee][fff(ggg)hhh)iii]'));
 });
 
 test('escapeWindowsFilename', () => {
--- packages/comic-spider/src/utils/stringUtils.ts
+++ packages/comic-spider/src/utils/stringUtils.ts
@@ -4,26 +4,40 @@ interface BracketString {
 }
 
 /**
- * 按照中括号分隔给定文本。
- * 例如：[aaa][bbb]ccc[ddd][eee]
+ * 按照中括号分隔给定文本。括号格式支持[]和().
+ *
+ * 对于部分不完备的括号，例如只有左括号或右括号的，则匹配为文本。
+ *
+ * 例如：[aaa][bbb]ccc[ddd][eee][fff(ggg)hhh)iii]
  * 分隔后的结果为：
- *    [ { isBracket: true, text: '[aaa]' },
- *      { isBracket: true, text: '[bbb]' },
- *      { isBracket: false, text: 'ccc' },
- *      { isBracket: true, text: '[ddd]' },
- *      { isBracket: true, text: '[eee]' } ]
+ * [ { isBracket: true, text: '[aaa]' },
+ *  { isBracket: true, text: '[bbb]' },
+ *  { isBracket: false, text: 'ccc' },
+ *  { isBracket: true, text: '[ddd]' },
+ *  { isBracket: true, text: '[eee]' },
+ *  { isBracket: false, text: '[fff' },
+ *  { isBracket: true, text: '(ggg)' },
+ *  { isBracket: false, text: 'hhh' },
+ *  { isBracket: false, text: ')' },
+ *  { isBracket: false, text: 'iii' },
+ *  { isBracket: false, text: ']' } ]
+ *
  * @param str
  */
 export function splitByBracket(str: string): BracketString[] {
     if (!str.trim()) return [];
 
-    const txt = str.replace('【', '[').replace('】', ']');
+    // eslint-disable-next-line prettier/prettier
+    const txt = str.replace('【', '[')
+        .replace('】', ']')
+        .replace('（', '(')
+        .replace('）', ')');
 
     // 使用正则表达式按照中括号分隔字符串
     // 例如 [aaa][bbb]ccc[ddd][eee] 将会分隔为：
     // ["[", "aaa", "]", "[", "bbb", "]", "ccc", "[", "ddd", "]", "[", "eee", "]"]
     const arr = txt
-        .split(/([\[\]])/)
+        .split(/([\[\]()])/)
         .filter(s => !!s.trim())
         .map(s => s.trim());
 
@@ -33,21 +47,31 @@ export function splitByBracket(str: string): BracketString[] {
     const newArr: BracketString[] = [];
     for (let i = 0; i < len; i++) {
         const item = arr[i];
-        if (item === '[') {
+        if (item === '[' || item === '(') {
+            if (bracketItem) {
+                newArr.push({isBracket: false, text: bracketItem});
+            }
             bracketStart = true;
             bracketItem = item;
             continue;
         }
-        if (item === ']') {
-            bracketStart = false;
+        if (item === ']' || item === ')') {
             bracketItem += item;
-            newArr.push({isBracket: true, text: bracketItem});
-
+            if (bracketStart) {
+                bracketStart = false;
+                newArr.push({isBracket: true, text: bracketItem});
+            } else {
+                newArr.push({isBracket: false, text: bracketItem});
+            }
+            bracketItem = '';
             continue;
         }
 
         if (bracketStart) {
             bracketItem += item;
+            if (i === len - 1) {
+                newArr.push({isBracket: false, text: bracketItem});
+            }
         } else {
             newArr.push({isBracket: false, text: item});
         }
